2fda483faa5e2dc6f707560215d1e8107ae761a4
joyal89 <shahjoyal4@gmail.com>
2016-12-15 01:55:16 -0500

    Server.js Change

--- neu/webdev/server.js
+++ neu/webdev/server.js
@@ -6,13 +6,11 @@ var connectionString = 'mongodb://root:admin@ds155087.mlab.com:55087/wam-fall-20
 mongoose.connect(connectionString);
 
 var bodyParser = require('body-parser');
-app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/json'}));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // configure a public directives to host static content
 app.use(express.static(__dirname + '/public'));
-
-
 
 //require("./assignment/app.js")(app);
 require("./project/app.js")(app);
